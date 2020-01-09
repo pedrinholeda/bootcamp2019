2af568a4e1b737ad280a5174f3b06af7bee674f9
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-01-09 14:11:52 -0300

    CRUD

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,10 +2,50 @@ const express = require("express");
 
 const server = express();
 
-server.get("/users/:id", (req, res) => {
-  const { id } = req.params;
+server.use(express.json());
 
-  return res.json({ message: `Buscando usuario ${id}` });
+const users = ["Pedro", "Thallys", "Joao", "Guilherme"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  // insomnia http://localhost:3000/users
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  // insomnia http://localhost:3000/users -> passando o nome em json
+
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  // insomnia http://localhost:3000/users/1 -> passando o name em json
+
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  // insomnia http://localhost:3000/users/1
+
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.json(users);
 });
 
 server.listen(3000);
